a21e8fd1744f4dfc32ae34f0d0b363ef9017379b
Manolya Tamimi <tamimimanolya@gmail.com>
2023-07-17 06:31:25 +0300

    removing nav bar for now

--- utils/nav.php
+++ utils/nav.php
@@ -1,6 +1,6 @@
 <?php
 echo "<nav>";
-    echo "<a href='.'> click me </a>";
+    // echo "<a href='./feed'> click me </a>";
     // echo "<a href='../pages/home.php'> | home | </a>";
     // echo "<a href='../pages/insert.php'>insert | </a>";
     echo "</nav>";
