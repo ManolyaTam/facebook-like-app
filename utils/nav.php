82e45da1d2e97daac22246cab28200143a924774
Manolya Tamimi <tamimimanolya@gmail.com>
2023-07-17 06:33:15 +0300

    removing dbcon message

--- utils/nav.php
+++ utils/nav.php
@@ -1,6 +1,6 @@
 <?php
 echo "<nav>";
-    // echo "<a href='./feed'> click me </a>";
+    // echo "<a href='./feed.php'> click me </a>";
     // echo "<a href='../pages/home.php'> | home | </a>";
     // echo "<a href='../pages/insert.php'>insert | </a>";
     echo "</nav>";
